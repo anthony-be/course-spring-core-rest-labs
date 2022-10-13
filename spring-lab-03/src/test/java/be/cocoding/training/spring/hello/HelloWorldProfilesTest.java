9cf0596b645d145bec254dec291791b03ad716a3
Anthony <29755086+anthony-be@users.noreply.github.com>
2022-10-13 05:48:39 +0200

    Fixed HelloWorldProfilesTest class of Lab 2 - Exercice 3

--- spring-lab-03/src/test/java/be/cocoding/training/spring/hello/HelloWorldProfilesTest.java
+++ spring-lab-03/src/test/java/be/cocoding/training/spring/hello/HelloWorldProfilesTest.java
@@ -1,40 +1,49 @@
 package be.cocoding.training.spring.hello;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration("classpath:spring/application-context.xml")
-@Ignore("Need helloWorldProfile bean definition from Lab 2 - Exercice 3")
+@RunWith(Suite.class)
+@Suite.SuiteClasses({HelloWorldProfilesTest.WithTestProfile.class, HelloWorldProfilesTest.WithoutTestProfile.class})
+//@Ignore("Need helloWorldProfile bean definition from Lab 2 - Exercice 3")
 public class HelloWorldProfilesTest {
 
-    @Autowired
-    private ApplicationContext context;
+    @RunWith(SpringRunner.class)
+    @ContextConfiguration("classpath:spring/application-context.xml")
+    @ActiveProfiles("test")
+    public static class WithTestProfile {
 
-    @Test
-    @Profile("test")
-    public void loadContextWithProfileTest(){
-        HelloWorld hw = getHelloWorldProfileBean();
-        assertEquals("Student name for profile test is not valid","TEST", hw.getStudent());
+        @Autowired
+        private ApplicationContext context;
+
+        @Test
+        public void loadContextWithProfileTest(){
+            HelloWorld hw = context.getBean("helloWorldProfile", HelloWorld.class);
+            assertEquals("Student name for profile test is not valid","TEST", hw.getStudent());
+        }
     }
 
-    @Test
-    @Profile("dummy")
-    public void loadContextWithoutProfileTest(){
-        HelloWorld hw = getHelloWorldProfileBean();
-        assertEquals("Student name for profile NOT test is not valid","NON TEST", hw.getStudent());
-    }
+    @RunWith(SpringRunner.class)
+    @ContextConfiguration("classpath:spring/application-context.xml")
+    @ActiveProfiles("dummy")
+    public static class WithoutTestProfile {
 
-    private HelloWorld getHelloWorldProfileBean(){
-        return context.getBean("helloWorldProfile", HelloWorld.class);
+        @Autowired
+        private ApplicationContext context;
+
+        @Test
+        public void loadContextWithProfileTest(){
+            HelloWorld hw = context.getBean("helloWorldProfile", HelloWorld.class);
+            assertEquals("Student name for profile NOT test is not valid","NON TEST", hw.getStudent());
+        }
     }
 
 }
